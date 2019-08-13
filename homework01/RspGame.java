283da25212894f4ad596d6f53352c5d57a8516f4
erinyoonchoi <thisiserinchoi@gmail.com>
2019-08-13 18:59:28 +0900

    8/13/2019 18:59

--- homework01/RspGame.java
+++ homework01/RspGame.java
@@ -10,33 +10,33 @@ public class RspGame {
 		int mix = ran.nextInt(3)+1;
 		System.out.println("가위/바위/보 중 하나를 입력하세요.");
 		String client = scan.next();
-		String eval = "";
+		String result = "";
 		//컴퓨터(랜덤값)	
-		String auto = "";
+		String computer = "";
 		switch(mix) {
-		case 1: auto ="가위";
+		case 1: computer ="가위";
 				switch(client){
-				case "가위": eval ="비김"; break;
-				case "바위": eval = "승리" ; break;
-				case "보": eval = "패배"; break;
+				case "가위": result ="비김"; break;
+				case "바위": result = "승리" ; break;
+				case "보": result = "패배"; break;
 				}
 		break;
-		case 2: auto ="바위";
+		case 2: computer ="바위";
 				switch(client){
-				case "가위": eval ="패배"; break;
-				case "바위": eval = "비김" ; break;
-				case "보": eval = "승리"; break;
+				case "가위": result ="패배"; break;
+				case "바위": result = "비김" ; break;
+				case "보": result = "승리"; break;
 				}
 		break;
-		case 3: auto ="보";
+		case 3: computer ="보";
 				switch(client){
-				case "가위": eval ="승리"; break;
-				case "바위": eval = "패배" ; break;
-				case "보": eval = "비김"; break;
+				case "가위": result ="승리"; break;
+				case "바위": result = "패배" ; break;
+				case "보": result = "비김"; break;
 				}
 		break;
 			}
-		System.out.println(eval);
+		System.out.println(result);
 		}
 		
 	}
